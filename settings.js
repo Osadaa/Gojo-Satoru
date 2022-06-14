83bf063ead2d6abd9b56b7184fd0dfd12de07803
Osadaa <107493385+Osadaa@users.noreply.github.com>
2022-06-14 21:17:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,17 +19,17 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94752088386']
+global.premium = ['94752088386']
+global.ownernomer = '94752088386'
+global.ownername = '🐦OSADA BRO'
+global.botname = 'OSADA BOT'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
+global.myweb = 'https://youtube.com/channel/UCrKsUaN2z5Y9ZpISBiVtNjA'
+global.packname = 'OSADA BRO'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
